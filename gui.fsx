a529661d95b8469577e67620f2cdfe1b6c050f67
youssef <yusefhany093@gmail.com>
2024-12-13 01:51:45 +0200

    remove getUserInput function

--- gui.fsx
+++ gui.fsx
@@ -9,25 +9,6 @@ let loadTextFromFile (filePath: string) =
     with
     | ex -> sprintf "Error reading file: %s" ex.Message
 
-// this is user interface function to determine the method
-let getUserInput () =
-    printfn "Choose input method: 1 - Direct Text, 2 - upload File using path"
-    match Console.ReadLine() with
-    | "1" -> 
-        printfn "Enter your text:"
-        Console.ReadLine()
-    | "2" -> 
-        printfn "Enter the file path:"
-        let filePath = Console.ReadLine()
-        if File.Exists filePath then
-            loadTextFromFile(filePath)
-        else
-            printfn "File not found. Exiting."
-            ""
-    | _ -> 
-        printfn "Invalid choice. Exiting."
-        ""
-
 // this functoin to clean text from special character
 let cleanText (text: string) =
    // Keep "F#" as a special case and remove other punctuation
@@ -77,3 +58,4 @@ let displayResults (text: string) =
    |> Seq.take 10
    |> Seq.iter (fun (word, count) -> printfn "%s: %d" word count)
    printfn "\nAverage Sentence Length: %.2f" (averageSentenceLength text)
+
